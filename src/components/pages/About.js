7cbbaf4b13539f8be3b2bfb7fac4bf0c48c06b98
Rhebeca Abreu <rhebecaabreu@gmail.com>
2022-06-17 10:05:08 -0300

    Add About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,10 +5,15 @@ import Hero from "components/molecules/Hero";
 import Heading from "components/atoms/Heading";
 import Section from "components/molecules/Section";
 import Grid from "components/atoms/Grid";
+import Card, { CardMedia, CardMediaDescription } from "components/atoms/Card";
 
 import HeroImage from "assets/gestao-de-frota.jpg";
 import AboutImage from "assets/draws/About";
 import Footer from "components/organisms/Footer";
+import Instructor1 from "assets/instructors/01-cat.jpg";
+import Instructor2 from "assets/instructors/02-cat.jpg";
+import Instructor3 from "assets/instructors/03-cat.jpg";
+import Instructor4 from "assets/instructors/04-cat.jpg";
 
 const ImageContainer = styled.div`
   svg {
@@ -18,6 +23,13 @@ const ImageContainer = styled.div`
     color: ${(props) => props.theme.colors.primary.main};
   }
 `;
+
+const instructors = [
+  { id: 1, name: "Thor", avatar: Instructor1 },
+  { id: 2, name: "Mel", avatar: Instructor2 },
+  { id: 3, name: "Mike", avatar: Instructor3 },
+  { id: 4, name: "Bob", avatar: Instructor4 },
+];
 
 const About = () => (
   <>
@@ -75,11 +87,21 @@ const About = () => (
       <Grid md={2}>
         <div>
           <h4>Missão</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis praesentium natus delectus, animi, modi obcaecati rem dolore porro ipsa similique harum eaque impedit laborum cumque aliquam consectetur enim? Quasi, odio.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            praesentium natus delectus, animi, modi obcaecati rem dolore porro
+            ipsa similique harum eaque impedit laborum cumque aliquam
+            consectetur enim? Quasi, odio.
+          </p>
         </div>
         <div>
           <h4>Visão</h4>
-          <p>Provident itaque aliquid, fugiat iure deleniti corrupti officia harum aperiam reiciendis, nam perferendis porro dolorem nisi sequi dolore iste error repudiandae! Optio totam consequuntur aperiam tempora sit distinctio hic. Ex.</p>
+          <p>
+            Provident itaque aliquid, fugiat iure deleniti corrupti officia
+            harum aperiam reiciendis, nam perferendis porro dolorem nisi sequi
+            dolore iste error repudiandae! Optio totam consequuntur aperiam
+            tempora sit distinctio hic. Ex.
+          </p>
         </div>
       </Grid>
     </Section>
@@ -87,6 +109,18 @@ const About = () => (
       <Heading>
         <h2>Conheça nossos professores</h2>
       </Heading>
+
+      <Grid sm={2} md={3} lg={4}>
+        {instructors.map((instructor) => (
+          <Card key={instructor.id}>
+            <CardMedia image={instructor.avatar}>
+              <CardMediaDescription>
+                <h6>{instructor.name}</h6>
+              </CardMediaDescription>
+            </CardMedia>
+          </Card>
+        ))}
+      </Grid>
     </Section>
     <Footer />
   </>
