75f8cd24b5f56dde8f5afcedc9ca7ea4d0869c48
Rhebeca Abreu <rhebecaabreu@gmail.com>
2022-06-17 23:25:38 -0300

    Fix dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,16 @@
 module.exports = {
-  stories: [
-    "../src/stories/**/*.stories.@(js|mdx)",
-    "../src/components/**/*.stories.@(js|mdx)",
-    "../src/styles/**/*.stories.@(js|mdx)",
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-actions",
+  "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-viewport",
-    {
-      name: "@storybook/addon-docs",
-      options: {
-        configureJSX: true,
-      },
-    },
-    "@storybook/addon-knobs",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app"
   ],
-};
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
